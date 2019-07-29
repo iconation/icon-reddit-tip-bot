f6d2f7466332f356f1b5d3964653fe0fe5f9f0e9
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2019-07-29 09:43:51 +0300

    Fixes card misalignment

--- resources/views/user/withdraw.blade.php
+++ resources/views/user/withdraw.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-2"></div>
+        <div class="col-md-3"></div>
         <div class="col-md-6">
             <div class="card mb-8">
                 <div class="card-header">
